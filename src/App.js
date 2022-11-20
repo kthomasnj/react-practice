b69dff92512f39e99158eb2cbec70e62bd4f4205
Keith S Thomas <kthomasnj@hotmail.com>
2022-11-20 12:20:23 -0500

    Cleaning up code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Nav from './components/Nav';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
 
 function App() {
   return <Nav />;
 }
-
-<Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/About" element={<About />} />
-  <Route path="/Contact" element={<Contact />} />
-</Routes>
 
 export default App;
